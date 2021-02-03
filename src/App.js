99c48f0d64c8ee696a803e7daa275d2f28845d30
j <j@e.com>
2021-02-03 18:45:22 -0500

    first commit in ppc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App.js updated
+          Page from PPC Branch
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
