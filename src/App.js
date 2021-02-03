b3ec91029f12016affc8ce9335af1611df632f38
j <j@e.com>
2021-02-03 18:27:31 -0500

    New text added in dev branh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App.js updated
+          You are see the page from development branch
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
