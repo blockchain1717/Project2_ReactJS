9878e17128b067da42041c7db0cf28aba94e65d0
j <j@e.com>
2021-02-03 18:33:00 -0500

    text updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          You are see the page from development branch
+          You are seeing the page from development branch
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
